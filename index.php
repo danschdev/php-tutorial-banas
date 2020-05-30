e058a4f8e867e51388fa2a341ed5e254734dfb54
Daniel Schnelle <dschnelle@gmx.net>
2020-05-30 15:43:35 +0200

    Add basic form

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ $l_name = "Schnelle";
 $age = 35;
 $height = 1.84;
 $can_vote = true;
-$address = array('street'=>'Oertzweg 10',
-'city'=>'Hamburg');
+$address = array(
+	'street' => 'Oertzweg 10',
+	'city' => 'Hamburg'
+);
 $state = NULL;
 define('PI', 3.14159)
 ?>
@@ -19,6 +21,28 @@ define('PI', 3.14159)
 
 <body>
 	<p>Name: <?php echo $f_name . ' ' . $l_name; ?></p>
+	<form action="index.php" method="get">
+		<label>Your state : </label>
+		<input type="text" name="state" /><br />
+		<label>Number 1 : </label>
+		<input type="text" name="num-1" /><br />
+		<label>Number 2 : </label>
+		<input type="text" name="num-2" /><br />
+		<input type="submit" value="Submit"/>
+	</form>
+	<?php
+		if(isset($_GET) && array_key_exists('state', $_GET)){
+			$state=$_GET['state'];
+			if (isset($state) && !empty($state)) {
+				echo 'You live in ' . $state . '<br/>';
+			}
+			if (count($_GET) >=3 ) {
+				$num_1 = $_GET['num-1'];
+				$num_2 = $_GET['num-2'];
+				echo "$num_1 + $num_2 = ". ($num_1 + $num_2) . "<br/>";
+			}
+		}
+	?>
 </body>
 
 </html>
