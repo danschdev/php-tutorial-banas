49001f056d7f02c5d16a3683c2329062282a449a
Daniel Schnelle <dschnelle@gmx.net>
2020-05-30 15:23:10 +0200

    Add basic PHP index document

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+$f_name = "Daniel";
 ?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<title>PHP Tutorial</title>
+</head>
+
+<body>
+	<p><?php echo $f_name ?></p>
+</body>
+
+</html>
