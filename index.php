c5f7d0094c1a20648eb49b59d873e431cc4a96bd
Daniel Schnelle <dschnelle@gmx.net>
2020-05-30 15:28:04 +0200

    Add basic variables and constant

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 $f_name = "Daniel";
+$l_name = "Schnelle";
+$age = 35;
+$height = 1.84;
+$can_vote = true;
+$address = array('street'=>'Oertzweg 10',
+'city'=>'Hamburg');
+$state = NULL;
+define('PI', 3.14159)
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
@@ -10,7 +18,7 @@ $f_name = "Daniel";
 </head>
 
 <body>
-	<p><?php echo $f_name ?></p>
+	<p>Name: <?php echo $f_name . ' ' . $l_name; ?></p>
 </body>
 
 </html>
