1fcb9815e6d291748f34ed2ec48e51ab3a26f158
Daniel Schnelle <dschnelle@gmx.net>
2020-06-07 10:38:14 +0200

    Add form with number operations

--- index.php
+++ index.php
@@ -28,20 +28,29 @@ define('PI', 3.14159)
 		<input type="text" name="num-1" /><br />
 		<label>Number 2 : </label>
 		<input type="text" name="num-2" /><br />
-		<input type="submit" value="Submit"/>
+		<input type="submit" value="Submit" />
 	</form>
 	<?php
-		if(isset($_GET) && array_key_exists('state', $_GET)){
-			$state=$_GET['state'];
-			if (isset($state) && !empty($state)) {
-				echo 'You live in ' . $state . '<br/>';
-			}
-			if (count($_GET) >=3 ) {
-				$num_1 = $_GET['num-1'];
-				$num_2 = $_GET['num-2'];
-				echo "$num_1 + $num_2 = ". ($num_1 + $num_2) . "<br/>";
+	if (isset($_GET) && array_key_exists('state', $_GET)) {
+		$state = $_GET['state'];
+		if (isset($state) && !empty($state)) {
+			echo 'You live in ' . $state . '<br/>';
+		}
+		if (count($_GET) >= 3) {
+			$num_1 = $_GET['num-1'];
+			$num_2 = $_GET['num-2'];
+			echo "$num_1 + $num_2 = " . ($num_1 + $num_2) . "<br/>";
+			echo "$num_1 - $num_2 = " . ($num_1 - $num_2) . "<br/>";
+			echo "$num_1 * $num_2 = " . ($num_1 * $num_2) . "<br/>";
+			echo "$num_1 / $num_2 = " . ($num_1 / $num_2) . "<br/>";
+			echo "$num_1 / $num_2 ~ " . intdiv($num_1, $num_2) . "<br/>";
+			echo "$num_1 % $num_2 = " . ($num_1 % $num_2) . "<br/>";
+			echo "Increment $num_1 = "; {
+				$num_1++;
+				echo $num_1 . "<br/>";
 			}
 		}
+	}
 	?>
 </body>
 
